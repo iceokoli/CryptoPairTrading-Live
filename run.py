831ce80fb8eed23ee871aeec7afcb5d4ba607128
Ikenna  Okoli <iceokoli@yahoo.com>
2021-03-30 22:54:53 +0100

    switch to using aiohttp

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
+import asyncio
 import json
-import time
-import ssl
 import logging
 import datetime
 import os
+import sys
 from pathlib import Path
+from argparse import ArgumentParser
 
-import websocket
+import aiohttp
 
 from account import Account
 from strategy import PairsStrategy
@@ -14,31 +15,32 @@ from utility import estimate_mid_price
 
 dir = str(Path(__file__).parent)
 
-logging.basicConfig(
-    format="%(asctime)s:%(levelname)s: - %(message)s",
-    level=logging.INFO,
-    filemode="a",
-    filename=f"{dir}/logging/logging-{datetime.datetime.now().date()}.txt",
-)
+if sys.platform == "darwin":
+    logging.basicConfig(
+        format="%(asctime)s:%(levelname)s: - %(message)s",
+        level=logging.INFO,
+    )
+else:
+    logging.basicConfig(
+        format="%(asctime)s:%(levelname)s: - %(message)s",
+        level=logging.INFO,
+        filemode="a",
+        filename=f"{dir}/logging/logging-{datetime.datetime.now().date()}.txt",
+    )
+
 
 logger = logging.getLogger(__name__)
 
 btc_mid, eth_mid = float(), float()
 
-client_id = os.getenv("BSID")
-auth = {
-    "secret": bytes(os.getenv("BSSECRET"), "utf-8"),
-    "key": os.getenv("BSKEY"),
-}
 
-with open(f"{dir}/aggregates.json", "r") as f:
-    agg = json.load(f)
+def handle_input():
 
-acc = Account(auth, client_id, logger)
+    parser = ArgumentParser()
+    parser.add_argument("--mode", required=True, choices=["DEV", "PROD"])
+    args = parser.parse_args()
 
-strat = PairsStrategy(
-    account=acc, enter_trigger=1, exit_trigger=0.1, agg_data=agg, logger=logger
-)
+    return args.mode
 
 
 def store_data(mid, channel):
@@ -49,36 +51,26 @@ def store_data(mid, channel):
         btc_mid = mid
     elif channel == "order_book_ethgbp":
         eth_mid = mid
-    # elif channel == "order_book_ethbtc":
-    #     cross_mid = mid
 
 
-def on_open(ws):
+async def on_open(ws):
     btc_sub = json.dumps(
         {"event": "bts:subscribe", "data": {"channel": "order_book_btcgbp"}}
     )
     eth_sub = json.dumps(
         {"event": "bts:subscribe", "data": {"channel": "order_book_ethgbp"}}
     )
-    # cross_sub = json.dumps(
-    #     {"event": "bts:subscribe", "data": {"channel": "order_book_ethbtc"}}
-    # )
-    ws.send(btc_sub)
-    ws.send(eth_sub)
+    await ws.send_str(btc_sub)
+    await ws.send_str(eth_sub)
     logger.info("### subscribed ###")
 
 
-def on_close(ws):
-
-    logger.info("### closed ###")
-
-
-def on_message(ws, message):
+async def on_message(ws, message, strat):
 
     global btc_mid, eth_mid
 
     try:
-        data = json.loads(message)
+        data = json.loads(message.data)
         event = data["event"]
         channel = data["channel"]
         logger.info(f"### received message: {event}, {channel} ###")
@@ -91,7 +83,7 @@ def on_message(ws, message):
 
         if eth_mid and btc_mid:
             s = strat.calc_spread(btc_mid, eth_mid)
-            strat.evaluate_action(s)
+            await strat.evaluate_action(s)
             btc_mid, eth_mid = float(), float()
 
     except Exception as error:
@@ -99,19 +91,36 @@ def on_message(ws, message):
         ws.close()
 
 
-def on_error(ws, error):
-    logger.info(error)
+async def main():
+
+    mode = handle_input()
+
+    client_id = os.getenv("BSID")
+    auth = {
+        "secret": bytes(os.getenv("BSSECRET"), "utf-8"),
+        "key": os.getenv("BSKEY"),
+    }
+
+    with open(f"{dir}/aggregates.json", "r") as f:
+        agg = json.load(f)
+
+    acc = Account(auth, client_id, logger, mode)
+
+    strat = PairsStrategy(
+        account=acc, enter_trigger=1, exit_trigger=0.1, agg_data=agg, logger=logger
+    )
+
+    data_feed = "wss://ws.bitstamp.net/"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(data_feed) as ws:
+            await on_open(ws)
+            async for msg in ws:
+                await on_message(ws, msg, strat)
 
 
 if __name__ == "__main__":
 
-    data_feed = "wss://ws.bitstamp.net/"
-    # websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(
-        data_feed,
-        on_close=on_close,
-        on_error=on_error,
-        on_open=on_open,
-        on_message=on_message,
-    )
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
